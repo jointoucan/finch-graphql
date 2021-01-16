0444a38a3ff5cfb001cc9f748b42ef8dc84b398d
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-01-16 14:29:53 -0800

    add in better exports, and bump version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from "./background";
 export * from "./client";
 export * from "./types";
+export { ExtensionProvider, useExtension } from "./hooks/ExtensionProvider";
+export { useQuery } from "./hooks/useQuery";
+export { useMutation } from "./hooks/useMutation";
