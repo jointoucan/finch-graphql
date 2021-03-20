35ec425c8981437828fa9f0c5ceb7a825ca74e43
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-03-20 11:40:11 -0700

    add in some comments and uuid for request ids

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,17 @@
 import { DocumentNode, GraphQLFormattedError } from 'graphql';
 import gql from 'graphql-tag';
 import { sendMessage } from './browser';
+import { isListeningOnDocument, queryApiFromDocument } from './external';
 import { GenericVariables, FinchMessageKey, FinchQueryOptions } from './types';
 import { isDocumentNode } from './utils';
 
+/**
+ * messageCreator is a function that create a finch message to send to the background process
+ * @param query the graphQL query to send to api
+ * @param variables the variables for the graphQL query.
+ * @param messageKey optional message key that will get sent to background script
+ * @returns the custom message object for this query
+ */
 const messageCreator = <Variables extends GenericVariables = {}>(
   query: string | DocumentNode,
   variables: Variables,
@@ -16,6 +24,17 @@ const messageCreator = <Variables extends GenericVariables = {}>(
   };
 };
 
+/**
+ * queryApi is a methods that adds a sugary syntax on-top of browser.messaging
+ * for Finch GraphQL. This makes it so you can call this method without having to
+ * worry about how the message is getting sent.
+ * @param query a string or graphQL Document Node query
+ * @param variables the variables for the graphQL query.
+ * @param options set of options for configuring this query.
+ * @param options.messageKey a custom message key to send to the extension
+ * @param options.id an id of the extension to send the query to.
+ * @returns a promise that resolves a graphQL response.
+ */
 export const queryApi = async <
   Query extends {} = {},
   Variables extends GenericVariables = {}
@@ -31,11 +50,13 @@ export const queryApi = async <
 
   if (extensionId) {
     args.unshift(extensionId);
+    if (isListeningOnDocument()) {
+      return queryApiFromDocument<Query, Variables>(query, variables);
+    }
   }
 
-  const resp = sendMessage<{
+  return sendMessage<{
     data: Query | null;
     errors?: GraphQLFormattedError[];
   }>(...args);
-  return resp;
 };
