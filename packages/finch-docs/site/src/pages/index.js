73f52c61ed16352a7b2131716f7df364d0371859
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-03-27 14:07:47 -0700

    add in documentation on typescript codegen

--- packages/finch-docs/site/src/pages/index.js
+++ packages/finch-docs/site/src/pages/index.js
@@ -70,6 +70,11 @@ function Home() {
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={clsx(styles.codeBlock, styles.buttons)}>
+            <pre>
+              <code>npm install finch-graphql --save</code>
+            </pre>
+          </div>
           <div className={styles.buttons}>
             <Link
               className={clsx(
