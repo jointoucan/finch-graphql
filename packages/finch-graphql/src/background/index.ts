2c76aec30ad3e21587e725a72474ec045f87729e
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-06-04 08:38:08 -0500

    fix test in new exports

--- packages/finch-graphql/src/background/index.ts
+++ packages/finch-graphql/src/background/index.ts
@@ -1,2 +1,7 @@
 export { FinchApi } from './FinchApi';
 export { NoIntrospection } from './NoIntrospection';
+export {
+  finchDevtoolsOnResponse,
+  finchDevtoolsResolvers,
+  finchDevtoolsSchema,
+} from './FinchDevtools';
