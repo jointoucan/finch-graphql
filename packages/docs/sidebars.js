e299cad6c1c2b7d44cbf87e7fd037a1314385a2e
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-09-20 15:52:55 -0700

    fix sidebar

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -8,6 +8,6 @@ module.exports = {
       'website-setup',
     ],
     ['Further integrations']: ['devtools', 'typescript-codegen'],
-    ['Whats new?']: ['finch-v2.x'],
+    ['Whats new?']: ['finch-v2'],
   },
 };
