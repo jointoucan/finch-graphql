f405a8244333d069de0d1588d1035202d17f5f15
Jacob Lowe <jacoblowe2.0@gmail.com>
2021-12-05 17:26:21 -0600

    add in some new referance pages and remove schema from react

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -7,6 +7,7 @@ module.exports = {
       'client-setup',
       'website-setup',
     ],
+    ['API Reference']: ['api-ref', 'client-ref', 'react-ref'],
     ['Further integrations']: ['devtools', 'typescript-codegen'],
     ['Whats new?']: ['finch-v2'],
   },
