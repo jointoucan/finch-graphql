d62bfc65e42a8a5a70e43e83dddb277da40f22c0
Jacob Lowe <jacoblowe2.0@gmail.com>
2022-01-26 13:27:16 -0800

    Export new listener to allow usage from react package (#270)
    * Publish
    
     - finch-graphql-devtools@1.7.0
     - finch-docs@2.2.0
     - @finch-graphql/api@3.1.0
     - @finch-graphql/browser-polyfill@3.1.0
     - @finch-graphql/client@3.1.0
     - @finch-graphql/react@3.1.0
     - @finch-graphql/types@3.1.0
     - finch-typescript-codegen@3.1.0
    
    * export listener to allow access top level

--- packages/client/src/external/index.ts
+++ packages/client/src/external/index.ts
@@ -1,3 +1,4 @@
 export { listenForDocumentQueries } from './listenForDocumentQueries';
 export { queryApiFromDocument } from './queryApiFromDocument';
 export { isListeningOnDocument } from './isListeningOnDocument';
+export { listenForExternalRequests } from './listenForExternalRequests';
